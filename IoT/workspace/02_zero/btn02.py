70efb12612a8c1e9e0cefa226074d7727e6fb758
eunjijen <75493219+eunjijen@users.noreply.github.com>
2023-07-04 20:23:20 +0900

    Delete IoT directory

--- IoT/workspace/02_zero/btn02.py
+++
@@ -1,7 +0,0 @@
-from gpiozero import Button
-
-button = Button(26)
-
-button.wait_for_press()
-
-print("Button was pressed")
--- IoT/workspace/googleSpeech/tts01.py
+++
@@ -1,5 +0,0 @@
-from gtts import gTTS
-
-text = "Hello, 안녕하세요."
-tts= gTTS(text=text, lang='ko')
-tts.save('hello.mp3')
